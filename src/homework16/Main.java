54eb9cd80e270a1ad1e1f46e2a92d1e5a7d25809
nadiaslobodian <by.hope.mail@gmail.com>
2020-06-26 22:02:44 -0500

    Changes in Main

--- src/homework16/Main.java
+++ src/homework16/Main.java
@@ -4,9 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         int[] array1 = {153, 12, 65, 149, 2, 46, 75, 19, 33};
-
         int[] array2 = {3, 6, 1, 25, 89, 2, 7, 15};
-
         int[] array3 = {9, 5, 19, 29, 33, 59, 1, 7, 99};
 
         Data.sort(array1);
